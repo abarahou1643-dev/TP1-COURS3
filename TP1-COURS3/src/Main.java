01ab3bc0a7cffab48452d88f898b379e7f72e027
abarahou1643-dev <a.barahou1643@uca.ac.ma>
2025-10-19 21:56:10 +0100

    Add files via upload

---
+++ TP1-COURS3/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("TP Reflection - Programmation Dynamique");
+        System.out.println("Exécutez Presentation2 pour voir le résultat");
+    }
+}
